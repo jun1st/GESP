9c4bc04262010951c49cde151816fc0233165ba0
feng qijun <jun1st.feng@gmail.com>
2026-08-02 18:05:05 +0800

    新增主页与学习乐园更新：Tailwind 主页、编辑器行号/自动保存、GESP 真题示例

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"PingFang SC"', '"Hiragino Sans GB"', '"Microsoft YaHei"', '"Noto Sans SC"', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
